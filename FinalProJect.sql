ea2e64290392789e01484aa5ae11028f46283f28
Logan Northern <65001138+Starssk1ttles@users.noreply.github.com>
2022-06-29 19:41:25 -0500

    Add files via upload

---
+++ FinalProJect.sql
@@ -0,0 +1,28 @@
+use crypto;
+show tables;
+
+INSERT INTO crypto (symbol, name, blockchain_id, currentprice) 
+values ('BTC', 'BITCOIN', 'PRIV', '2106'),
+('ETH', 'ETHEREUM', 'PUB', '1195'),
+('XRP', 'RIPPLE', 'PRIV', '.37'),
+('LTC', 'LITECOIN', 'FED', '57.88'),
+('BNB', 'BNB', 'PUB', '234'),
+('SOL', 'SOLANA', 'FED', '40.36'),
+('AVX', 'AVALANCHE', 'FED', '20.28'),
+('LEO', 'UNUS SED LEO', 'PRIV', '5.92'),
+('FTT', 'FTX TOKEN' , 'PRIV', '27.36'),
+('XMR', 'MONERO', 'PUB', '122.71'),
+('ETC', 'ETHEREUM CLASSIC', 'FED', '16.10'),
+('HNT', 'HELIUM', 'PRIV', '11.43'),
+('MKR', 'MAKER', 'FED', '1011.73'),
+('ZEC', 'ZCASH', 'PRIV', '65.84'),
+('GRT', 'THE GRAPH', 'PUB', '0.11'),
+('QNT', 'QUANT', 'PUB', '62.44'),
+('KSM', 'KUSAMA', 'FED', '55.65'),
+('AR', 'ARWEAVE', 'FED', '10.25'),
+('GT', 'GATETOKEN', 'PRIV', '4.26'),
+('GN', 'GNOSIS', 'FED',  '122.87'),
+('CVX', 'CONVEX FINANCE', 'PUB', '4.77'),
+('ILV', 'ILLUVIUM', 'FED', '175.55'),
+('YFI', 'YEARNFINANCE', 'FED', '6633.11'),
+('ZEN', 'HORIZEN', 'PRIV',  '15.61');
